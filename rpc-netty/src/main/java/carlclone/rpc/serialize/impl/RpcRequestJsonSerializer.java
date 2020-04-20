ebc7d2a345d9765cb900857ae383e05ca6a11c47
lincongcong <lincongcong@tangbull.com>
2020-04-20 13:08:32 +0800

    update

--- rpc-netty/src/main/java/carlclone/rpc/serialize/impl/RpcRequestJsonSerializer.java
+++ rpc-netty/src/main/java/carlclone/rpc/serialize/impl/RpcRequestJsonSerializer.java
@@ -16,57 +16,25 @@ import java.util.List;
 public class RpcRequestJsonSerializer implements Serializer<RpcRequest> {
     @Override
     public int size(RpcRequest request) {
-        JSONObject object = new JSONObject();
-        //string
-        object.put("interfaceName",request.getInterfaceName());
-        //int
-        object.put("methodName",request.getMethodName());
-        //boolean
-        object.put("serializeArgs",request.getSerializedArguments());
-
-        String jstr = object.toJSONString();
-
-        return jstr.getBytes().length;
-
+        String str = JSON.toJSONString(request);
+        return str.getBytes(StandardCharsets.UTF_8).length;
     }
 
     @Override
     public void serialize(RpcRequest request, byte[] bytes, int offset, int length) {
-        ByteBuffer buffer = ByteBuffer.wrap(bytes, offset, length);
-        JSONObject object = new JSONObject();
-        //string
-        object.put("interfaceName",request.getInterfaceName());
-        //int
-        object.put("methodName",request.getMethodName());
-        //boolean
-        object.put("serializeArgs",new String(request.getSerializedArguments()));
-
-        String jstr = object.toJSONString();
-        bytes = jstr.getBytes();
-        buffer.put(bytes);
-
-        //array
-//        List<Integer> integers = Arrays.asList(1,2,3);
-//        object.put("list",integers);
-        //null
-//        object.put("null",null);​
-//        System.out.println(object);
+        String str = JSON.toJSONString(request);
+        byte[] strBytes = str.getBytes(StandardCharsets.UTF_8);
+        System.arraycopy(strBytes, 0, bytes, offset, strBytes.length);
 
     }
 
     @Override
     public RpcRequest parse(byte[] bytes, int offset, int length) {
 
-//        byte[] objBytes = new byte[length];
-//        System.arraycopy(bytes, offset, objBytes, 0, length);
-//        String jsonStr = new String(objBytes, StandardCharsets.UTF_8);
-//        JSONObject object = JSON.parseObject(jsonStr);
-//        return new RpcRequest(object.getString("interfaceName"),object.getString("methodName"),object.getString("serializeArgs").getBytes());
-//
-        String jstr = new String(bytes);
-        jstr = jstr.substring(1);
-        JSONObject object = JSON.parseObject(jstr);
-        return new RpcRequest(object.getString("interfaceName"),object.getString("methodName"),object.getString("serializeArgs").getBytes());
+        byte[] objBytes = new byte[length];
+        System.arraycopy(bytes, offset, objBytes, 0, length);
+        String jsonStr = new String(objBytes, StandardCharsets.UTF_8);
+        return (RpcRequest) JSON.parse(jsonStr);
     }
 
     @Override
